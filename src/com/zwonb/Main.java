e303556d061868a934cbc7f851f1e8e1041c744b
zwonb <1140114702@qq.com>
2018-04-09 16:44:55 +0800

    保留小数点后两位

--- src/com/zwonb/Main.java
+++ src/com/zwonb/Main.java
@@ -1,15 +1,21 @@
 package com.zwonb;
 
 import java.text.DecimalFormat;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(0.001f);
-        System.out.println(10000000f);
-        //DecimalFormat用于格式化十进制数字
-        DecimalFormat decimalFormat = new DecimalFormat();
-        decimalFormat.applyPattern("#,##0.00'\u00A4'");
-        System.out.println(decimalFormat.format(0.4d));
+        double m = 5325.55;
+        double km = m / 1000;
+        System.out.println(km);
+        // 保留小数点后两位，四舍五入
+        System.out.println(String.format(Locale.CHINA, "%.2f", km));
+//        System.out.println(0.001f);
+//        System.out.println(10000000f);
+//        //DecimalFormat用于格式化十进制数字
+//        DecimalFormat decimalFormat = new DecimalFormat();
+//        decimalFormat.applyPattern("#,##0.00'\u00A4'");
+//        System.out.println(decimalFormat.format(0.4d));
     }
 }
